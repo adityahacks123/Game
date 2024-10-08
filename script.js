ebbeb833a9c4bec37cf54719be7a2a765d9b1e8e
adityahacks123 <adityasingh30112020@gmail.com>
2024-10-08 17:02:10 +0530

    taking html elements and initializing values

---
+++ script.js
@@ -0,0 +1,9 @@
+const gameContainer = document.getElementById('game-container');
+const robot = document.getElementById('robot');
+const rocksContainer = document.getElementById('rocks');
+const scoreboard = document.getElementById('scoreboard');
+
+let robotX = 0;
+let robotSpeed = 5;
+let score = 0;
+let gameInterval;
